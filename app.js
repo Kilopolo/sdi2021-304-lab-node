7b721c6b6d1fc6f307ab3b23d24de7c60bec2e47
UO271245 <71383208+UO271245@users.noreply.github.com>
2021-03-26 07:23:31 +0100

    SDI-2021-304-7.5-Vistas y motores de plantillas.

--- app.js
+++ app.js
@@ -7,13 +7,15 @@ app.use(express.static('public'));
 let bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+// incluir el motor de plantillas en la aplicación
+let swig = require('swig');
 
 // Variables
 app.set('port', 8081);
 
 //Rutas/controladores por lógica
-require("./routes/rusuarios.js")(app); // (app, param1, param2, etc.)
-require("./routes/rcanciones.js")(app); // (app, param1, param2, etc.)
+require("./routes/rusuarios.js")(app,swig); // (app, param1, param2, etc.)
+require("./routes/rcanciones.js")(app,swig); // (app, param1, param2, etc.)
 
 // Lanzar el servidor
 app.listen(app.get('port'), function(){
--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -1,24 +1,52 @@
-module.exports = function(app) {
-    app.get("/canciones", function(req, res) {
+module.exports = function (app, swig) {
 
-        let respuesta = "";
-        if (req.query.nombre != null)
-            respuesta+= 'Nombre: ' + req.query.nombre + '<br>' ;
-        if(typeof (req.query.autor) != "undefined")
-            respuesta+= 'Autor: '+ req.query.autor;
+    /*
+    Nota: el método GET /canciones/agregar tiene que ir antes de /canciones/:id. El
+    orden en el que escribimos los métodos GET y POST es importante puesto que indican
+    PRIORIDAD
+     */
+    //posibilidad de responder a la petición GET /canciones/agregar
+    app.get('/canciones/agregar', function (req, res) {
+        let respuesta = swig.renderFile('views/bagregar.html', {});
         res.send(respuesta);
-
-
     });
-    app.get('/suma', function(req, res) {
+
+    app.get("/canciones", function (req, res) {
+        // crearemos un array con 3 canciones
+        let canciones = [{
+            "nombre": "Blank space",
+            "precio": "1.2"
+        }, {
+            "nombre": "See you again",
+            "precio": "1.3"
+        }, {
+            "nombre": "Uptown Funk",
+            "precio": "1.1"
+        }];
+        //retorno de la función será el HTML final procesado por la plantilla
+        let respuesta = swig.renderFile('views/btienda.html', {
+            vendedor: 'Tienda de canciones',
+            canciones: canciones
+        });
+        // let respuesta = "";
+        // if (req.query.nombre != null)
+        //     respuesta+= 'Nombre: ' + req.query.nombre + '<br>' ;
+        // if(typeof (req.query.autor) != "undefined")
+        //     respuesta+= 'Autor: '+ req.query.autor;
+        res.send(respuesta);
+    });
+
+    app.get('/suma', function (req, res) {
         let respuesta = parseInt(req.query.num1) + parseInt(req.query.num2);
         res.send(String(respuesta));
     });
-    app.get('/canciones/:id', function(req, res) {
+
+    app.get('/canciones/:id', function (req, res) {
         let respuesta = 'id: ' + req.params.id;
         res.send(respuesta);
     });
-    app.get('/canciones/:genero/:id', function(req, res) {
+
+    app.get('/canciones/:genero/:id', function (req, res) {
         let respuesta = 'id: ' + req.params.id + '<br>'
             + 'Género: ' + req.params.genero;
         res.send(respuesta);
@@ -26,10 +54,10 @@ module.exports = function(app) {
 
     //procese los atributos nombre, género y
     // precio enviados a través del formulario.
-    app.post("/cancion", function(req, res){
-    res.send("Canción agregada:"+req.body.nombre +"<br>"
-        +" genero" + req.body.genero +"<br>"
-        +" precio: "+ req.body.precio);
+    app.post("/cancion", function (req, res) {
+        res.send("Canción agregada:" + req.body.nombre + "<br>"
+            + " genero" + req.body.genero + "<br>"
+            + " precio: " + req.body.precio);
     });
 
     app.get('/promo*', function (req, res) {
