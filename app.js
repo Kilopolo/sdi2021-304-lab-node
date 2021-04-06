94d8e6a6c1fd9ba1daf45982dbd529b45a0f3e6c
UO271245 <71383208+UO271245@users.noreply.github.com>
2021-04-07 00:06:30 +0200

    SDI-2021-304-8.1-Acceso a datos de Mongo desde Node.js.

--- app.js
+++ app.js
@@ -1,23 +1,34 @@
 // Módulos
 let express = require('express');
 let app = express();
+
+let mongo = require('mongodb');
 //, declaramos la ruta public como estática en app.js
 app.use(express.static('public'));
 //añadimos el require del módulo “body-parser”
 let bodyParser = require('body-parser');
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 // incluir el motor de plantillas en la aplicación
 let swig = require('swig');
 
 // Variables
 app.set('port', 8081);
+app.set('db', 'mongodb://admin:sdi@tiendamusica-shard-00-00.any5v.mongodb.net:27017,' +
+    'tiendamusica-shard-00-01.any5v.mongodb.net:27017,' +
+    'tiendamusica-shard-00-02.any5v.mongodb.net:27017' +
+    '/myFirstDatabase?ssl=true' +
+    '&replicaSet=atlas-exyqcf-shard-0' +
+    '&authSource=admin' +
+    '&retryWrites=true' +
+    '&w=majority');
+
 
 //Rutas/controladores por lógica
-require("./routes/rusuarios.js")(app,swig); // (app, param1, param2, etc.)
-require("./routes/rcanciones.js")(app,swig); // (app, param1, param2, etc.)
-require("./routes/rautores.js")(app,swig); // (app, param1, param2, etc.)
+require("./routes/rusuarios.js")(app, swig); // (app, param1, param2, etc.)
+require("./routes/rcanciones.js")(app, swig, mongo); // (app, param1, param2, etc.)
+require("./routes/rautores.js")(app, swig); // (app, param1, param2, etc.)
 // Lanzar el servidor
-app.listen(app.get('port'), function(){
+app.listen(app.get('port'), function () {
     console.log('Servidor activo');
 });
